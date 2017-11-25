2fe64a863e4a4ae071ee945275162bdf73e140d7
Antonio <antonio.pauletich95@gmail.com>
2017-11-25 12:43:47 +0100

    WIP

--- tests/Feature/TorrentUploadServiceTest.php
+++ tests/Feature/TorrentUploadServiceTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Http\Models\Torrent;
 use App\Http\Models\User;
 use App\Http\Services\BdecodingService;
 use App\Http\Services\BencodingService;
+use App\Http\Services\SizeFormattingService;
+use App\Http\Services\TorrentInfoService;
 use Illuminate\Support\Facades\App;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Storage;
@@ -30,6 +33,11 @@ class TorrentUploadServiceTest extends TestCase
         $encoderStub = $this->createMock(BencodingService::class);
         App::instance(BencodingService::class, $encoderStub);
 
+        $infoService = $this->createMock(TorrentInfoService::class);
+        $torrentSize = 5000;
+        $infoService->method('getTorrentSize')->willReturn($torrentSize);
+        App::instance(TorrentInfoService::class, $infoService);
+
         $torrentValue = '123456';
         $encoderStub->method('encode')->willReturn($torrentValue);
 
@@ -45,5 +53,12 @@ class TorrentUploadServiceTest extends TestCase
         // Assert the file was stored...
         Storage::disk('public')->assertExists('torrents/1.torrent');
         $this->assertSame($torrentValue, Storage::disk('public')->get('torrents/1.torrent'));
+
+        $formatter = new SizeFormattingService();
+
+        $torrent = Torrent::find(1);
+        $this->assertSame($formatter->getFormattedSize($torrentSize), $torrent->size);
+        $this->assertSame('Test', $torrent->name);
+        $this->assertSame('Test', $torrent->description);
     }
 }
