3932a5dcd12ae91522feb5fd9df9704c0a54a9a6
Antonio <antonio.pauletich95@gmail.com>
2017-12-27 23:30:30 +0100

    WIP

--- resources/views/torrents/create.blade.php
+++ resources/views/torrents/create.blade.php
@@ -12,24 +12,28 @@
             <br>
             <form enctype="multipart/form-data" method="POST" action="{{ route('torrents.store') }}">
                 {{ csrf_field() }}
+
                 <div class="form-group row">
-                    <label for="torrent" class="col custom-file">
-                        <input type="file" id="torrent" name="torrent" class="custom-file-input" onchange="getName()" accept="application/x-bittorrent" required>
-                        <span id="torrentName" class="custom-file-control form-control-file">{{ __('messages.torrent.create.choose_torrent_file') }}</span>
-                    </label>
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="torrent" name="torrent" onchange="getName()" accept="application/x-bittorrent" required>
+                        <label class="custom-file-label" for="torrent">{{ __('messages.torrent.create.choose_torrent_file') }}</label>
+                    </div>
                 </div>
+
                 <div class="form-group row">
                     <label for="name" class="col-sm-2 col-form-label">{{ __('messages.torrent.create.torrent_name') }}</label>
                     <div class="col-sm-10">
                         <input type="text" class="form-control" id="name" name="name" placeholder="{{ __('messages.torrent.create.torrent_name') }}" value="{{ old('name') }}" required>
                     </div>
                 </div>
+
                 <div class="form-group row">
                     <label for="description" class="col-sm-2 col-form-label">{{ __('messages.torrent.create.torrent_description') }}</label>
                     <div class="col-sm-10">
                         <textarea id="description" name="description" class="form-control" placeholder="{{ __('messages.torrent.create.torrent_description') }}" rows="10" required>{{ old('description') }}</textarea>
                     </div>
                 </div>
+
                 <button type="submit" class="btn btn-primary">{{ __('messages.torrent.create.upload_button_text') }}</button>
             </form>
         </div>
@@ -38,5 +42,5 @@
 @endsection
 
 @section('scripts')
-    <script src="{{asset('js/getName.js')}}"></script>
+    <script src="{{ asset('js/getName.js') }}"></script>
 @endsection
