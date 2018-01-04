70a1a502239044f8a888e30816465e101df25b44
X-Coder264 <antonio.pauletich95@gmail.com>
2018-01-04 01:33:10 +0100

    WIP

--- app/Http/Services/TorrentUploadService.php
+++ app/Http/Services/TorrentUploadService.php
@@ -64,10 +64,6 @@ class TorrentUploadService
 
         $decodedTorrent = $this->decoder->decode($torrentContent);
 
-        $bytes = random_bytes(64);
-        // add entropy to randomize info_hash in order to prevent peer leaking attacks
-        $decodedTorrent['info']['entropy'] = bin2hex($bytes);
-
         // the torrent must be private
         $decodedTorrent['info']['private'] = 1;
 
@@ -76,12 +72,24 @@ class TorrentUploadService
         // TODO: add support for multiple announce URLs
         $decodedTorrent['announce'] = route('announce');
 
+        do {
+            $bytes = random_bytes(64);
+            // add entropy to randomize info_hash in order to prevent peer leaking attacks
+            // we are recalculating the entropy until we get an unique info_hash
+            $decodedTorrent['info']['entropy'] = bin2hex($bytes);
+
+            $infoHash = $this->getTorrentInfoHash($decodedTorrent['info']);
+
+            $torrent = Torrent::where('infoHash', '=', $infoHash)->select('infoHash')->first();
+
+        } while (null !== $torrent);
+
         $torrent = new Torrent();
         $torrent->name = $request->input('name');
         $torrent->size = $torrentSize;
         $torrent->description = $request->input('description');
         $torrent->uploader_id = Auth::id();
-        $torrent->infoHash = sha1($this->encoder->encode($decodedTorrent['info']));
+        $torrent->infoHash = $infoHash;
         if (true === $torrent->save()) {
             $stored = Storage::disk('public')->put(
                 "/torrents/{$torrent->id}.torrent",
@@ -97,5 +105,15 @@ class TorrentUploadService
         } else {
             throw new Exception();
         }
+    }
+
+    /**
+     * @param array $torrentInfoDictionary
+     *
+     * @return string
+     */
+    protected function getTorrentInfoHash(array $torrentInfoDictionary): string
+    {
+        return sha1($this->encoder->encode($torrentInfoDictionary));
     }
 }
--- tests/Feature/Services/TorrentUploadServiceTest.php
+++ tests/Feature/Services/TorrentUploadServiceTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Services;
 
 use Tests\TestCase;
 use App\Http\Models\User;
@@ -8,18 +8,17 @@ use App\Http\Models\Torrent;
 use Illuminate\Http\Response;
 use Illuminate\Http\Testing\File;
 use Illuminate\Http\UploadedFile;
-use App\Http\Services\BdecodingService;
 use App\Http\Services\BencodingService;
+use App\Http\Services\BdecodingService;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Services\TorrentInfoService;
+use App\Http\Services\TorrentUploadService;
 use App\Http\Services\SizeFormattingService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TorrentUploadServiceTest extends TestCase
 {
     use RefreshDatabase;
-
-    // TODO: add a test that shows that the info_hash must be unique in the DB (if it's not then a new entropy is calculated until it is)
 
     public function testTorrentUpload()
     {
@@ -48,8 +47,8 @@ class TorrentUploadServiceTest extends TestCase
         $torrentDescription = 'Test description';
 
         $response = $this->post(route('torrents.store'), [
-            'torrent' => File::create('file.torrent'),
-            'name' => $torrentName,
+            'torrent'     => File::create('file.torrent'),
+            'name'        => $torrentName,
             'description' => $torrentDescription,
         ]);
 
@@ -80,7 +79,7 @@ class TorrentUploadServiceTest extends TestCase
         Storage::fake('public');
 
         $torrentFile = new UploadedFile(
-            realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Fixtures' . DIRECTORY_SEPARATOR . 'non private torrent.torrent'),
+            realpath(__DIR__ . DIRECTORY_SEPARATOR),
             'non private torrent',
             'application/x-bittorrent',
             null,
@@ -96,8 +95,8 @@ class TorrentUploadServiceTest extends TestCase
         $torrentDescription = 'Test description';
 
         $response = $this->post(route('torrents.store'), [
-            'torrent' => $torrentFile,
-            'name' => $torrentName,
+            'torrent'     => $torrentFile,
+            'name'        => $torrentName,
             'description' => $torrentDescription,
         ]);
 
@@ -122,7 +121,7 @@ class TorrentUploadServiceTest extends TestCase
         Storage::fake('public');
 
         $torrentFile = new UploadedFile(
-            realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Fixtures' . DIRECTORY_SEPARATOR . 'test.torrent'),
+            realpath(__DIR__ . DIRECTORY_SEPARATOR),
             'test',
             'application/x-bittorrent',
             null,
@@ -134,8 +133,8 @@ class TorrentUploadServiceTest extends TestCase
         $torrentDescription = 'Test description';
 
         $response = $this->post(route('torrents.store'), [
-            'torrent' => $torrentFile,
-            'name' => $torrentName,
+            'torrent'     => $torrentFile,
+            'name'        => $torrentName,
             'description' => $torrentDescription,
         ]);
 
@@ -151,6 +150,69 @@ class TorrentUploadServiceTest extends TestCase
         $this->assertSame(128, strlen($decodedTorrent['info']['entropy']));
     }
 
+    public function testTorrentMustHaveAnUniqueInfoHash()
+    {
+        $infoHash = 'ccd285bd6d7fc749e9ed34d8b1e8a0f1b582d977';
+        factory(Torrent::class)->create(['infoHash' => $infoHash, 'seeders' => 0, 'leechers' => 0]);
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        Storage::fake('public');
+
+        $decoder = $this->createMock(BdecodingService::class);
+        $this->app->instance(BdecodingService::class, $decoder);
+
+        $decoder->method('decode')->willReturn(['test' => 'test']);
+
+        $encoder = $this->createMock(BencodingService::class);
+        $this->app->instance(BencodingService::class, $encoder);
+
+        $infoService = $this->createMock(TorrentInfoService::class);
+        $torrentSize = 5000;
+        $infoService->method('getTorrentSize')->willReturn($torrentSize);
+        $this->app->instance(TorrentInfoService::class, $infoService);
+
+        $torrentValue = '123456';
+        $encoder->method('encode')->willReturn($torrentValue);
+
+        $torrentUploadService = $this->getMockBuilder(TorrentUploadService::class)
+            ->setConstructorArgs([$encoder, $decoder, $infoService])
+            ->setMethods(['getTorrentInfoHash'])
+            ->getMock();
+        $expectedHash = 'test hash 264';
+        $torrentUploadService->expects($this->exactly(2))
+            ->method('getTorrentInfoHash')
+            ->will($this->onConsecutiveCalls($infoHash, $expectedHash));
+
+        $this->app->instance(TorrentUploadService::class, $torrentUploadService);
+
+        $torrentName = 'Test name';
+        $torrentDescription = 'Test description';
+
+        $response = $this->post(route('torrents.store'), [
+            'torrent'     => File::create('file.torrent'),
+            'name'        => $torrentName,
+            'description' => $torrentDescription,
+        ]);
+
+        $torrent = Torrent::findOrFail(2);
+
+        $response->assertStatus(Response::HTTP_FOUND);
+        $response->assertRedirect(route('torrents.show', $torrent));
+        $response->assertSessionHas('success');
+
+        Storage::disk('public')->assertExists('torrents/2.torrent');
+        $this->assertSame($torrentValue, Storage::disk('public')->get('torrents/2.torrent'));
+
+        $formatter = new SizeFormattingService();
+
+        $this->assertSame($torrentSize, (int) $torrent->getOriginal('size'));
+        $this->assertSame($formatter->getFormattedSize($torrentSize), $torrent->size);
+        $this->assertSame($torrentName, $torrent->name);
+        $this->assertSame($torrentDescription, $torrent->description);
+        $this->assertSame($expectedHash, $torrent->infoHash);
+    }
+
     public function testTorrentsHaveTheCorrectAnnounceUrl()
     {
         $this->withoutExceptionHandling();
@@ -161,7 +223,7 @@ class TorrentUploadServiceTest extends TestCase
         Storage::fake('public');
 
         $torrentFile = new UploadedFile(
-            realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Fixtures' . DIRECTORY_SEPARATOR . 'test.torrent'),
+            realpath(__DIR__ . DIRECTORY_SEPARATOR),
             'test',
             'application/x-bittorrent',
             null,
@@ -173,8 +235,8 @@ class TorrentUploadServiceTest extends TestCase
         $torrentDescription = 'Test description';
 
         $response = $this->post(route('torrents.store'), [
-            'torrent' => $torrentFile,
-            'name' => $torrentName,
+            'torrent'     => $torrentFile,
+            'name'        => $torrentName,
             'description' => $torrentDescription,
         ]);
 
