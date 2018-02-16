fc3876ee83a3c03b9abf4c3d7b7bffeca15a2056
Antonio <antonio.pauletich95@gmail.com>
2018-02-17 00:41:24 +0100

    WIP

--- tests/HashFake.php
+++ tests/HashFake.php
@@ -9,7 +9,7 @@ class HashFake implements HasherContract
     /**
      * Get information about the given hashed value.
      *
-     * @param  string $hashedValue
+     * @param string $hashedValue
      * @return array
      */
     public function info($hashedValue)
@@ -20,8 +20,8 @@ class HashFake implements HasherContract
     /**
      * Hash the given value.
      *
-     * @param  string $value
-     * @param  array  $options
+     * @param string $value
+     * @param array  $options
      * @return string
      */
     public function make($value, array $options = [])
@@ -32,9 +32,9 @@ class HashFake implements HasherContract
     /**
      * Check the given plain value against a hash.
      *
-     * @param  string $value
-     * @param  string $hashedValue
-     * @param  array  $options
+     * @param string $value
+     * @param string $hashedValue
+     * @param array  $options
      * @return bool
      */
     public function check($value, $hashedValue, array $options = [])
@@ -45,8 +45,8 @@ class HashFake implements HasherContract
     /**
      * Check if the given hash has been hashed using the given options.
      *
-     * @param  string $hashedValue
-     * @param  array  $options
+     * @param string $hashedValue
+     * @param array  $options
      * @return bool
      */
     public function needsRehash($hashedValue, array $options = [])
